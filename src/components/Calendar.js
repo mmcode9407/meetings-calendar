2ceb3ae0751806dd14d8363e70287346f2ab711c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-03-21 11:57:51 +0100

    create `Calendar` component

---
+++ src/components/Calendar.js
@@ -0,0 +1,50 @@
+﻿import React, { Component } from 'react';
+import { getData, addData, removeData } from '../calendarProvider';
+import CalendarForm from './CalendarForm';
+import CalendarList from './CalendarList';
+
+export default class Calendar extends Component {
+	state = {
+		meetings: [],
+	};
+
+	addMeeting = (data) => {
+		addData(data).then((data) => {
+			this.setState((state) => {
+				return {
+					meetings: [...state.meetings, data],
+				};
+			});
+		});
+	};
+
+	removeMeeting = (id) => {
+		removeData(id).then(
+			getData().then((data) => {
+				this.setState({
+					meetings: data,
+				});
+			})
+		);
+	};
+
+	componentDidMount() {
+		getData().then((data) => {
+			this.setState({
+				meetings: data,
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<CalendarForm onSubmit={this.addMeeting} />
+				<CalendarList
+					meetings={this.state.meetings}
+					onClick={this.removeMeeting}
+				/>
+			</div>
+		);
+	}
+}
