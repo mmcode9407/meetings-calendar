c788272750801f2be2d9e5d5535ec8ff5974a7c3
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-03-21 11:55:48 +0100

    create `CalendarList` component

---
+++ src/components/CalendarList.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+
+export default class CalendarList extends Component {
+	render() {
+		const { meetings, onClick } = this.props;
+		return (
+			<ul>
+				{meetings.map(({ firstName, lastName, email, date, time, id }) => {
+					return (
+						<li key={id}>
+							<h2>
+								{firstName} {lastName}
+							</h2>
+							<p>{email}</p>
+							<p>{date}</p>
+							<p>{time}</p>
+							<button onClick={() => onClick(id)}>Usuń spotkanie</button>
+						</li>
+					);
+				})}
+			</ul>
+		);
+	}
+}
