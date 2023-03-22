0211bbfd3d14e507c73a079c0cecfc5cce49dafc
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-03-22 09:42:53 +0100

    add patterns & required field

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -42,12 +42,19 @@ export default class Calendar extends Component {
 				<CalendarForm
 					onSubmit={this.addMeeting}
 					formItems={[
-						{ name: 'firstName', label: 'Imię', type: 'text', required: true },
+						{
+							name: 'firstName',
+							label: 'Imię',
+							type: 'text',
+							required: true,
+							pattern: '[a-z]{3,}',
+						},
 						{
 							name: 'lastName',
 							label: 'Nazwisko',
 							type: 'text',
 							required: true,
+							pattern: '[a-z]{3,}',
 						},
 						{
 							name: 'email',
@@ -56,8 +63,21 @@ export default class Calendar extends Component {
 							required: true,
 							pattern: '[0-9a-z_.-]+@[0-9a-z.-]+.[a-z]{2,3}',
 						},
-						{ name: 'date', label: 'Data', type: 'date', required: true },
-						{ name: 'time', label: 'Godzina', type: 'time', required: true },
+						{
+							name: 'date',
+							label: 'Data',
+							type: 'date',
+							required: true,
+							pattern:
+								'^([0-9]{4}[-/]?((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/]?02[-/]?29)$',
+						},
+						{
+							name: 'time',
+							label: 'Godzina',
+							type: 'time',
+							required: true,
+							pattern: '^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$',
+						},
 					]}
 				/>
 				<CalendarList
